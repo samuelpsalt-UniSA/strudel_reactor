253711cae33209fd3fb416c12224e5871be6f2f4
samuelpsalt-UniSA <salsp002@mymail.unisa.edu.au>
2025-10-30 13:17:46 +1030

    Implemented First Changes Video Changes

---
+++ strudel_reactor-main/src/components/PlayButtons.jsx
@@ -0,0 +1,10 @@
+function PlayButtons() {
+    return (
+        <>
+            <button id="play" className="btn btn-outline-primary">Play</button>
+            <button id="stop" className="btn btn-outline-primary">Stop</button>
+        </>
+    )
+}
+
+export default PlayButtons;
