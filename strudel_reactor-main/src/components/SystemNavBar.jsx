439a290219d6c99d0ea7410a306d125bfe0751e2
samuelpsalt-UniSA <salsp002@mymail.unisa.edu.au>
2025-11-18 15:28:51 +1030

    finished bootstrap import alert functions

--- strudel_reactor-main/src/components/SystemNavBar.jsx
+++ strudel_reactor-main/src/components/SystemNavBar.jsx
@@ -1,10 +1,23 @@
 import PlayButtons from "./PlayButtons";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {useState} from "react";
 
 export default function SystemNavBar({ourPlayButton, ourStopButton, onToggleEditor, isEditorVisible, onImportTune, onExportTune}){
 
     const buttonText = isEditorVisible ? "Hide Editor" : "Show Editor";
+
+    const [importAlert, setImportAlert] = useState(false);
+
+    const onImportClick = () => {
+        onImportTune();
+        setTimeout(() => setImportAlert(true), 3000);
+
+    }
+
+    const setImportAllertFalse = () => {
+        setImportAlert(false);
+    }
 
 
 
@@ -19,7 +32,7 @@ export default function SystemNavBar({ourPlayButton, ourStopButton, onToggleEdit
                             onClick={onToggleEditor}>
                             {buttonText}
                         </button>
-                        <button onClick={onImportTune}
+                        <button onClick={onImportClick}
                                 className="sticky-button">
                             Import Tune
                         </button>
@@ -34,10 +47,14 @@ export default function SystemNavBar({ourPlayButton, ourStopButton, onToggleEdit
                 </div>
             </nav>
 
-            <div className="alert alert-success alert-dismissible fade show" role="alert" id='importSuccessAlert'>
-                <strong>File Successfully Imported</strong>
-                <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
+            {importAlert && (
+                <div className="alert alert-success alert-dismissible fade show" role="alert" id='importSuccessAlert'>
+                    <strong>Tune Successfully Imported</strong>
+                    <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={setImportAllertFalse}></button>
+                </div>
+            )
+            };
+
         </>
 
     )
